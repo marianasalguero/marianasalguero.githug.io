4f911d089289a5045f52208f072560fd283c50b5
marianasalguero <126424774+marianasalguero@users.noreply.github.com>
2023-02-26 20:11:17 -0300

    Primer commit
    Adicion de archivos index ,style,funcion

---
+++ function.js
@@ -0,0 +1,22 @@
+
+ 
+   
+document.getElementById('boton1').onclick = function()  {
+    console.log("capturamos el evento click 1");
+    document.getElementById("demo1").innerHTML = "Diseñadora";
+};
+ 
+document.getElementById('boton2').onclick = function()  {
+    console.log("capturamos el evento click 2");
+    document.getElementById("demo2").innerHTML = "Buenos Aires";
+};
+ 
+document.getElementById('boton3').onclick = function()  {
+    console.log("capturamos el evento click 2");
+    document.getElementById("demo3").innerHTML = "mariana@gmail.com";
+};
+
+document.getElementById('boton4').onclick = function()  {
+    console.log("capturamos el evento click 2");
+    document.getElementById("demo4").innerHTML = "1127999999";
+}; 
